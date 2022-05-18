406a1f9de672c79345c98e8e9dd0f7579de1240b
harriet-kerubo <harrietkarori1@gmail.com>
2022-05-18 18:04:43 +0300

    Inspire Youth in STEM

--- lesson5.py
+++ lesson5.py
@@ -1,1 +1,32 @@
-#
+#methods
+name = "Harriet Kerubo"
+name = "Ada Lovelace"
+user_name = " Lovelace "
+age = 18
+age = 55
+# person = " I am " + str(name) " and I am amazing " +str(age)
+#print ("I am ") + str(name) + " and I am amazing " +str(age)
+# the format () method
+#print("My name is {} and I am {} years old ") format(name)
+#newline \n and tab \t
+#print (f"My \t name is {name} \n and I am (age) years old ")
+
+
+# Multiline strings
+msg = ''' QRST1010XDFG MPESA confirmed
+         you have received ksh50000 from
+         James Muthoki
+         18th May 2022
+         Safaricom transparent for you
+         '''
+
+print (msg)     
+txt = """ Holla!I am Kerubo Bacardi 
+        from JKUAT.I was asking if you'll 
+        be interested with our bootcamp"""  
+
+print (txt)          
+
+    
+
+
---
+++ slice.py
@@ -0,0 +1,44 @@
+city = "Nairobi"
+print(city[:5])
+print(city[2:])
+print(city[-1:])
+
+
+s_name = " HARRIET KERUBO"
+# .lower() convert to lower case
+print(s_name.lower())
+
+f_name = "harriet kerubo"
+# .upper() convert to upper case
+print(f_name.upper())
+
+#concatination converting one data to another
+# integer to float
+number = 6
+print(str(number))
+
+x=4 #x is an integer
+print(float(x))
+
+y=3.21
+print(int(y))
+
+f_name = "Harriet"
+
+s_name = "Kerubo"
+
+full_name = f_name + s_name
+
+print("full name")
+
+#The replace() method replaces a string with another
+
+name = "Harriet Kerubo"
+#The split() method returns a list where 
+
+msg = "Hello from Harriet Kerubo how are you "
+print(msg.split())
+
+print(len(msg))
+
+
