6130fb01cb94f81bcdddff5d61599f35bd366fdc
harriet-kerubo <harrietkarori1@gmail.com>
2022-05-22 15:33:48 +0300

    personal info

--- lesson1.py
+++ lesson1.py
@@ -9,8 +9,11 @@ print('Hello World')
 #Multiple/many line
 print("my name is Harriet Kerubo")
 print("my hobby is playing football")
+print("I schooled in Precious Blood")
 print("my favourite novel is After and Before")
 print("my nickname is Kerry")
 print("my pets name is Snow")
-
+print("my favourite colour is indigo")
+print("my zodiac sign is sagittarius")
+print("I'm 5'7 tall")
 
