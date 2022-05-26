96397fc73725a8c7d83601bc2b7d4751629593c2
harriet-kerubo <harrietkarori1@gmail.com>
2022-05-26 10:07:38 +0300

    lesson10.py

--- lesson10.py
+++ lesson10.py
@@ -1,1 +1,42 @@
+#!/usr/bin/python
+
+print(range(0,20))
+
+for numbers in range(0,10):
+    if(numbers%2==0):
+        print(numbers)
+
+#sum of all even numbers btwn 0 and 50
+for numbers in range(0,50):
+    if(numbers %2 == 0):
+        sum_numbers = 0
+        sum_numbers = 1
+
+for numbers in range(0,20):
+    if(numbers %2 == 0):
+        sum_numbers = sum_numbers + numbers
+print(sum_numbers)
+
+#product of odd numbers btwn 0 and 20
+      
+
+
+#while
+num = 0
+while num<10:
+    print(num)
+    num = num + 2
+
+while num<10:
+    print(num)    
+    num = num + 1
+
+if(num%2==0):
+    print(num)    
+
+  
+
+
+
+
 
